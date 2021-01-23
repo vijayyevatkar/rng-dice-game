e5dc506940fde393a4a8bb348a8a39e5da1f153e
vijayyevatkar <50692348+vijayyevatkar@users.noreply.github.com>
2021-01-23 14:20:11 +0530

    Stage - 1 - Basic working 2 player game over the board

--- script.js
+++ script.js
@@ -1,1 +1,133 @@
 "use strict";
+
+setupNewGame();
+
+var scores,
+  scoreInThisRound,
+  activePlayer,
+  continueGame,
+  prevScore,
+  waiting,
+  play,
+  dice,
+  finalScore;
+var map = ["Player 1", "Player 2"];
+waiting = "Waiting for my turn!";
+play = "I will play now!";
+finalScore = 50;
+
+document.querySelector(".btn--new").addEventListener("click", setupNewGame);
+document.querySelector(".btn--roll").addEventListener("click", buttonRoll);
+document.querySelector(".btn--hold").addEventListener("click", buttonHold);
+
+function setupNewGame() {
+  console.log("Starting new game!");
+  continueGame = true;
+  scores = [0, 0];
+  scoreInThisRound = 0;
+  activePlayer = 0;
+  prevScore = 0;
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("current--0").textContent = 0;
+  document.getElementById("current--1").textContent = 0;
+  document.getElementById("score--0").textContent = 0;
+  document.getElementById("score--1").textContent = 0;
+  document.querySelector(".player--0").classList.remove("player--active");
+  document.querySelector(".player--1").classList.remove("player--active");
+  document.querySelector(".player--0").classList.remove("player--winner");
+  document.querySelector(".player--1").classList.remove("player--winner");
+  document.querySelector(".player--0").classList.add("player--active");
+  document.querySelector(".player--1").classList.remove("player--subtext--win");
+  document.querySelector(".player--0").classList.remove("player--subtext--win");
+  document.getElementById("sub-1").textContent = "Waiting for my turn!";
+  document.getElementById("sub-0").textContent = "Roll Dice!";
+}
+
+function buttonRoll() {
+  if (!continueGame) return;
+  var diceNum = Math.floor(Math.random() * 6) + 1;
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + diceNum + ".png";
+  if (diceNum === 1) {
+    document.getElementById("sub-" + activePlayer).textContent =
+      "Oh No! I rolled a 1 😐";
+    changeActivePlayer(0);
+  } else {
+    scoreInThisRound += diceNum;
+    let currentTotal = scoreInThisRound + prevScore;
+    if (currentTotal >= finalScore) {
+      endGame();
+    } else {
+      document.getElementById("sub-" + activePlayer).textContent =
+        "Yay! I rolled a " + diceNum;
+      let otherPlayer = activePlayer - 1;
+      if (otherPlayer < 0) otherPlayer = 1;
+      document.getElementById("sub-" + otherPlayer).textContent =
+        "Waiting for my turn!";
+    }
+    document.getElementById(
+      "current--" + activePlayer
+    ).textContent = scoreInThisRound;
+  }
+}
+
+function endGame() {
+  document
+    .querySelector(".player--" + activePlayer)
+    .classList.remove("player--active");
+  document
+    .querySelector(".player--" + activePlayer)
+    .classList.add("player--winner");
+  continueGame = false;
+  document.getElementById("score--" + activePlayer).textContent =
+    scoreInThisRound + prevScore;
+  document
+    .querySelector(".player--subtext")
+    .classList.add("player--subtext--win");
+  document.getElementById("sub-" + activePlayer).textContent =
+    "Yay! I won!! 🎉";
+  activePlayer ? (activePlayer = 0) : (activePlayer = 1);
+  document.getElementById("sub-" + activePlayer).textContent =
+    "Nooo! I lost ☹️";
+}
+
+function changeActivePlayer(keepScore) {
+  if (!continueGame) return;
+  document
+    .querySelector(".player--" + activePlayer)
+    .classList.remove("player--active");
+  prevScore = parseInt(
+    document.getElementById("score--" + activePlayer).textContent,
+    10
+  );
+  let addedScore = prevScore;
+  if (keepScore) addedScore = scoreInThisRound + prevScore;
+  document.getElementById("score--" + activePlayer).textContent = addedScore;
+  document.getElementById("current--" + activePlayer).textContent = 0;
+  activePlayer ? (activePlayer = 0) : (activePlayer = 1);
+  scoreInThisRound = 0;
+  if (keepScore)
+    document.getElementById("sub-" + activePlayer).textContent =
+      "It's my turn now.";
+  else {
+    document.getElementById("sub-" + activePlayer).textContent =
+      "Ha-ha 😂 it's my turn now.";
+  }
+  document
+    .querySelector(".player--" + activePlayer)
+    .classList.add("player--active");
+
+  prevScore = parseInt(
+    document.getElementById("score--" + activePlayer).textContent,
+    10
+  );
+}
+
+function buttonHold() {
+  if (!continueGame) return;
+  document.getElementById("sub-" + activePlayer).textContent =
+    "I will hold for now.";
+
+  changeActivePlayer(1);
+}
